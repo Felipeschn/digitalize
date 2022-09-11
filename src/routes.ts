32f957498d849c02a3b549de9857a30e227ee4c3
Felipe Schneider <demellobrl@gmail.com>
2022-09-11 19:45:10 -0300

    first commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,9 @@
 import { Router } from "express";
-import { createUserController } from "./useCases/CreateUser";
-import { findUserController } from "./useCases/FindUser";
 
 const router = Router();
 
-router.post("/users", (req, resp) => {
-  return createUserController.handle(req, resp);
+router.get("/route/:params", (req, resp) => {
+  return;
 });
-
-router.get("/users/:email", (req, resp) => {
-  return findUserController.handle(req, resp);
-});
-
-router.get("/abc", (req, res) => res.send("test"));
 
 export { router };
