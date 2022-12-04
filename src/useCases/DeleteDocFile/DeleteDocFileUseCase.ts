8865a409cb022d9b30d51b7b7d49b0b5b2c71725
Felipe Schneider <demellobrl@gmail.com>
2022-12-04 17:22:25 -0300

    delete object s3 bucket

--- src/useCases/DeleteDocFile/DeleteDocFileUseCase.ts
+++ src/useCases/DeleteDocFile/DeleteDocFileUseCase.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm";
 import { DocFile } from "../../entities/DocFile";
 import { IDeleteDocFileDTO } from "./DeleteDocFileDTO";
+import aws from "aws-sdk";
 
 export class DeleteDocFileUseCase {
   constructor(private docFileRepository: Repository<DocFile>) {}
@@ -13,6 +14,25 @@ export class DeleteDocFileUseCase {
     });
 
     if (!docFileExists) throw new Error("DocFile not found!");
+
+    if (docFileExists.bucketUrl.includes("https://")) {
+      const s3 = new aws.S3();
+
+      s3.deleteObject(
+        {
+          Bucket: process.env.BUCKET_NAME,
+          Key: docFileExists.bucketUrl.split("/")[3],
+        },
+        function (err, data) {
+          if (err) {
+            console.log(err);
+            throw new Error("Error delete S3 Object");
+          } else {
+            console.log(data);
+          }
+        }
+      );
+    }
 
     await this.docFileRepository
       .createQueryBuilder()
